081f819a00eb1fa56addc22972e1d19d7e7da29d
jalvesteffer <jason.alvesteffer@smoothstack.com>
2020-05-23 22:30:22 -0400

    Class template added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdministratorComponent } from "./administrator/administrator.component";
-import { LmserrorComponent } from "./lmserror/lmserror.component";
+import { BorrowerComponent } from "./borrower/borrower.component";
+import { LibrarianComponent } from "./librarian/librarian.component";
 import { HomeComponent } from "./home/home.component";
+import { LmserrorComponent } from "./lmserror/lmserror.component";
 import { LayoutComponent } from "./layout/layout.component";
-import { from } from 'rxjs';
 
 
 const routes: Routes = [
@@ -16,7 +17,27 @@ const routes: Routes = [
         path: "",
         redirectTo: "lms/home",
         pathMatch: "full",
-      }
+      },
+      {
+        path: "lms/home",
+        component: HomeComponent,
+      },
+      {
+        path: "lms/admin",
+        component: AdministratorComponent,
+      },
+      {
+        path: "lms/borrower",
+        component: BorrowerComponent,
+      },
+      {
+        path: "lms/librarian",
+        component: LibrarianComponent,
+      },
+      {
+        path: "**",
+        component: LmserrorComponent,
+      },
     ]
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdministratorComponent } from './administrator/administrator.component';
-import { LayoutComponent } from './layout/layout.component';
-import { LmserrorComponent } from './lmserror/lmserror.component';
+import { LibrarianComponent } from './librarian/librarian.component';
+import { BorrowerComponent } from './borrower/borrower.component';
+import { AuthorComponent } from './administrator/author/author.component';
+import { BookComponent } from './administrator/book/book.component';
 import { HomeComponent } from './home/home.component';
+import { LmserrorComponent } from './lmserror/lmserror.component';
+import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 
@@ -21,7 +24,11 @@ import { FooterComponent } from './layout/footer/footer.component';
     LmserrorComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BorrowerComponent,
+    AuthorComponent,
+    BookComponent,
+    LibrarianComponent
   ],
   imports: [
     BrowserModule,
