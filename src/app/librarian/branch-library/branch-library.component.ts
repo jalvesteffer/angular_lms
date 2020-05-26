8a8d28d7368440ff232085a503abfc1de1d2fcc6
lexieAA <43833670+lexieAA@users.noreply.github.com>
2020-05-26 16:35:25 -0700

    libraraian branch view and home done and added to route. library url added to environments, didn't edite or remove any urls in there.

--- src/app/librarian/branch-library/branch-library.component.ts
+++ src/app/librarian/branch-library/branch-library.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LmsService } from "../../common/services/lms.service";
+import { environment } from "../../../environments/environment";
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-branch-library',
@@ -6,10 +9,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./branch-library.component.css']
 })
 export class BranchLibraryComponent implements OnInit {
+  totalBranches: number;
+  branches: any;
+  today = new Date();
+  private modalRef: NgbModalRef;
+  errMsg: any;
+  closeResult: any;
+  selectedObj: any;
 
-  constructor() { }
+  constructor(private lmsService: LmsService, private modalService: NgbModal) { }
 
   ngOnInit() {
+    this.loadAllBranches();
   }
 
+  loadAllBranches() {
+    this.lmsService.getAll(`${environment.libraryUrl}${environment.readBranchesURI}`)
+      .subscribe((res) => {
+        this.branches = res;
+        this.totalBranches = this.branches.length;
+      },
+        (error) => {
+          ;
+        }
+      );
+  }
+
+  // deleteBranch(branchId) {
+
+  //   this.lmsService.deleteObj(`${environment.libraryUrl}${environment.deleteBranchesURI}`, branchId)
+  //     .subscribe((res) => {
+  //       this.loadAllBranches();
+  //     },
+  //       (error) => {
+  //         ;
+  //       }
+  //     );
+  // }
+
+  // updateBranch() {
+  //   this.lmsService.updateObj(`${environment.libraryUrl}${environment.updateBranchesURI}`, this.selectedObj)
+  //     .subscribe((res) => {
+  //       this.loadAllBranches();
+  //       this.modalService.dismissAll();
+  //     },
+  //       (error) => {
+  //         ;
+  //       }
+  //     );
+  // }
+
+  // open(content, obj) {
+  //   this.selectedObj = obj;
+  //   this.modalRef = this.modalService.open(content);
+  //   this.modalRef.result.then(
+  //     (result) => {
+  //       this.errMsg = "";
+  //       this.closeResult = `Closed with ${result}`;
+  //     },
+  //     (reason) => {
+  //       this.errMsg = "";
+  //       this.closeResult = `Dismissed`;
+  //     }
+  //   );
+  // }
 }
