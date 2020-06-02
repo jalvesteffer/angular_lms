1c711ff4727336b51d7bb6519795dfc7b1a20cd4
jalvesteffer <jason.alvesteffer@smoothstack.com>
2020-06-02 02:07:18 -0400

    Implemented Search for Admin Services

--- src/app/administrator/book/book.component.ts
+++ src/app/administrator/book/book.component.ts
@@ -5,6 +5,7 @@ import { environment } from "../../../environments/environment";
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms"
 import { log } from 'util';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Component({
   selector: 'app-book',
@@ -25,6 +26,7 @@ export class BookComponent implements OnInit {
   authors: any;
   books: any;
   publisher: any;
+  publisherVal: any; // used for null check in updateBook()
   genres: any;
 
   today = new Date();
@@ -37,6 +39,10 @@ export class BookComponent implements OnInit {
   authorsDropdownSettings: any;
   genresDropdownSettings: any;
   publishersDropdownSettings: any;
+
+  // Sort
+  searchForm: FormGroup;
+  searchString: string;
 
   // Pagination
   pager: any = {};
@@ -98,6 +104,35 @@ export class BookComponent implements OnInit {
       publisher: new FormControl(this.publisher),
       genres: new FormControl(this.genres)
     });
+
+    this.searchForm = new FormGroup({
+      searchString: new FormControl(this.searchString),
+    });
+  }
+
+  search() {
+    let searchString = this.searchForm.value.searchString;
+    let dash = "/";
+    if (searchString.length != "") {
+      this.lmsService
+        .getAll(
+          `${environment.appUrl}${environment.readBooksURI}${environment.likeURI}${dash}${searchString}`
+        )
+        .subscribe(
+          (res) => {
+            this.books = res;
+            this.totalBooks = this.books.length;
+            this.searchString = "";
+            this.setPage(1);
+          },
+          (error) => {
+            this.searchString = "";
+          }
+        );
+    } else {
+      this.searchString = "";
+      this.loadAllBooks();
+    }
   }
 
   ngAfterViewInit() { }
@@ -170,10 +205,17 @@ export class BookComponent implements OnInit {
   }
 
   updateBook() {
+    // check for null value before getting publisherId at [0] index
+    if (!this.updateBookForm.value.publisher) {
+      this.publisherVal = null;
+    } else {
+      this.publisherVal = this.updateBookForm.value.publisher[0].publisherId;
+    }
+
     const book = {
       bookId: this.updateBookForm.value.bookId,
       title: this.updateBookForm.value.title,
-      pubId: this.updateBookForm.value.publisher[0].publisherId,
+      pubId: this.publisherVal,
       authors: this.updateBookForm.value.authors,
       genres: this.updateBookForm.value.genres
     }
