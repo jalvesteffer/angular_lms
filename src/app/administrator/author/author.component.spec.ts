fc9ed34f425267b7c1b5af839c74c7530f83eb96
jalvesteffer <jason.alvesteffer@smoothstack.com>
2020-06-03 16:11:24 -0400

    Class example

--- src/app/administrator/author/author.component.spec.ts
+++ src/app/administrator/author/author.component.spec.ts
@@ -1,6 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AuthorComponent } from './author.component';
+import { LmsService } from '../../common/services/lms.service';
+import { PagerService } from '../../common/services/pager.service';
+import { Observable, from, of, observable, throwError } from "rxjs";
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from "@angular/forms";
+import { Pipe, PipeTransform } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
+import { NgMultiSelectDropDownModule } from "ng-multiselect-dropdown";
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  tick,
+  fakeAsync
+} from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+
+@Pipe({
+  name: "authorSort"
+})
+export class MockAuthorSortPipe implements PipeTransform {
+  transform(input: any[]): any { }
+}
 
 describe('AuthorComponent', () => {
   let component: AuthorComponent;
@@ -8,9 +35,18 @@ describe('AuthorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AuthorComponent ]
+      declarations: [AuthorComponent, MockAuthorSortPipe],
+      imports: [
+        NgbModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        NgMultiSelectDropDownModule
+      ],
+      providers: [
+        LmsService, PagerService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
